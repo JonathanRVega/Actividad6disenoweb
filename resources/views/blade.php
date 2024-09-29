93e96279c73932ea7363c9f291c10d0668e5c627
JonathanRVega <Jonathaan.rv@gmail.com>
2024-09-29 02:11:39 -0600

    Productos Base

---
+++ resources/views/blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Vista del producto</title>
+        <title>Edición de Producto</title>
+        <title>Creación de Nuevo Producto</title>
+    </head>
+    <body>
+    </body>
+</html>
